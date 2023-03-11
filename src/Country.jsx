a3a5a40eb282d35247edce64af972b500ff7bc37
TadasV <tadasv1998@gmail.com>
2023-03-11 16:10:25 +0200

    added country body elements

--- src/Country.jsx
+++ src/Country.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 function Country() {
   return (
-    <div>Country</div>
+    <div className='country'>
+        <div className='flag_container'>
+            <img src="" alt="" />
+        </div>
+        <div className='details'>
+            <h3 className='name'>Name</h3>
+            <p>
+                Population: <span className='values'>Test</span>
+            </p>
+            <p>
+                Region: <span className='values'>Test</span>
+            </p>
+            <p>
+                Capital: <span className='values'>Test</span>
+            </p>
+        </div>
+    </div>
   )
 }
 
